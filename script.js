e710b1f076515ec1be1e5ada40c667414542402a
Nikita Zhavoronkov <larkgronick@gmail.com>
2019-06-27 17:20:57 +0300

    refactor: A code change that elements looks correct

--- script.js
+++ script.js
@@ -3,11 +3,3 @@ $(document).ready(function(){
         $(".menu a").fadeToggle(1000);
 });
 });
-
-$('a[href*=#scroll]').bind("click", function(e){ 
-    var anchor = $(this); 
-    $('html, body').stop().animate({ 
-    scrollTop: $(anchor.attr('href')).offset().top-60 
-    }, 1200); 
-    e.preventDefault(); 
-    }); 
